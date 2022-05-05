206af9344403d5a939f3071dc23eb71b7b6ad218
Andrea Watson <99840213+watsmeow@users.noreply.github.com>
2022-05-04 19:30:47 -0700

    Delete Count_Divisors.js

--- Count_Divisors.js
+++
@@ -1,9 +0,0 @@
-function countDivisors(num) {
-    let newarr = []
-    for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
-            newarr.push(i)
-        }
-    }
-    return newarr.length
-}
