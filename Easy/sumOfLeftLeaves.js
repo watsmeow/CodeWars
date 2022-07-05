9da3389fe982c840c6f79ba3b7e9d643dd394387
Andrea Watson <99840213+watsmeow@users.noreply.github.com>
2022-07-05 19:00:08 -0400

    Create sumOfLeftLeaves.js

---
+++ Easy/sumOfLeftLeaves.js
@@ -0,0 +1,9 @@
+var sumOfLeftLeaves = function(root) {
+    if (root == null) {
+        return 0
+    }
+    if (root.left != null && root.left.left == null && root.left.right == null) {
+        return root.left.val + sumOfLeftLeaves(root.right)
+    }
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+};
